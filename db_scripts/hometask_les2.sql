91baef989840e0616b69de1c2c4790dcdbe089b9
Alesya Khoroneko <horoneko00@gmail.com>
2020-03-31 08:29:01 +0300

    Refactor inserting data

--- db_scripts/hometask_les2.sql
+++ db_scripts/hometask_les2.sql
@@ -1,4 +1,3 @@
-drop database nc_project;
 create database nc_project;
 
 use nc_project;
@@ -43,73 +42,51 @@ create table nc_references(
             foreign key (list_value_id) references nc_list_values(list_value_id));
             
 insert into nc_object_types
-values(null, 'Internet Order Object Type', null);
-insert into nc_object_types
-values(null, 'Video Order Object Type', null);
-insert into nc_object_types
-values(null, 'Mobile Order Object Type', null);
-insert into nc_object_types
-values(null, 'Abstract Order Object Type', 'Abstract object type for all porduct orders');
-insert into nc_object_types
-values(null, 'Internet Instance Object Types', null);
-insert into nc_object_types
-values(null, 'Video Instance Object Types', null);
-insert into nc_object_types
-values(null, 'Mobile Order Object Type', null);
-insert into nc_object_types
-values(null, 'Abstract Instance Object Types', 'Abstract object type for all porduct instances');
-insert into nc_object_types
-values(null, 'Phone number', null);
+values
+		(null, 'Internet Order Object Type', null),
+		(null, 'Video Order Object Type', null),
+		(null, 'Mobile Order Object Type', null),
+		(null, 'Abstract Order Object Type', 'Abstract object type for all porduct orders'),
+		(null, 'Internet Instance Object Types', null),
+		(null, 'Video Instance Object Types', null),
+		(null, 'Mobile Order Object Type', null),
+		(null, 'Abstract Instance Object Types', 'Abstract object type for all porduct instances'),
+		(null, 'Phone number', null);
 
 insert into nc_objects
-values(null, 1, 'Internet Order #1', null);
-insert into nc_objects
-values(null, 2, 'Video Order #1', null);
-insert into nc_objects
-values(null, 2, 'Video Order #2', null);
-insert into nc_objects
-values(null, 1, 'Internet Order #2', null);
-insert into nc_objects
-values(null, 3, 'Mobile Order #1', null);
-insert into nc_objects
-values(null, 9, '07454343676', 'Number in the system');
+values
+		(null, 1, 'Internet Order #1', null),
+		(null, 2, 'Video Order #1', null),
+		(null, 2, 'Video Order #2', null),
+		(null, 1, 'Internet Order #2', null),
+		(null, 3, 'Mobile Order #1', null),
+		(null, 9, '07454343676', 'Number in the system');
 
 insert into nc_attributes
-values(null, 4, 'Due Date', 4);
-insert into nc_attributes
-values(null, 3, 'Phone Number', 9);
-insert into nc_attributes
-values(null, 1, 'Access Type', 6);
-insert into nc_attributes
-values(null, 1, 'Download Speed', 0);
-insert into nc_attributes
-values(null, 3, 'Service Type', 6);
-insert into nc_attributes
-values(null, 2, 'Suspend Reason', 0);
+values
+		(null, 4, 'Due Date', 4),
+		(null, 3, 'Phone Number', 9),
+		(null, 1, 'Access Type', 6),
+		(null, 1, 'Download Speed', 0),
+		(null, 3, 'Service Type', 6),
+		(null, 2, 'Suspend Reason', 0);
 
 insert into  nc_list_values
-values(null, 'XDSL');
-insert into  nc_list_values
-values(null, 'GPON');
-insert into  nc_list_values
-values(null, 'Postpaid');
-insert into  nc_list_values
-values(null, 'Prepaid');
+values
+		(null, 'XDSL'),
+		(null, 'GPON'),
+		(null, 'Postpaid'),
+		(null, 'Prepaid');
 
 insert into nc_params
-values(1, 3, 2, null);
-insert into nc_params
-values(1, 4, null, '100 Mbps');
-insert into nc_params
-values(2, 6, null, "Device Stolen");
-insert into nc_params
-values(3, 6, null, 'Tariff Change');
-insert into nc_params
-values(4, 3, 1, null);
-insert into nc_params
-values(4, 4, null, '500 Mbps');
-insert into nc_params
-values(5, 5, 3, null);
+values
+		(1, 3, 2, null),
+		(1, 4, null, '100 Mbps'),
+		(2, 6, null, "Device Stolen"),
+		(3, 6, null, 'Tariff Change'),
+		(4, 3, 1, null),
+		(4, 4, null, '500 Mbps'),
+		(5, 5, 3, null);
             
 insert into nc_references
 values(5, 2, 6);		
